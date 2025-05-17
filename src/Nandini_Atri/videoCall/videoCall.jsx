21c59fb41efe194cb06f4fbcc4f0f1a4ea28b01f
Nandiniatri <atrinandini8@gmail.com>
2025-05-17 15:30:02 +0530

    add react icon

--- src/Nandini_Atri/videoCall/videoCall.jsx
+++ src/Nandini_Atri/videoCall/videoCall.jsx
@@ -1,7 +1,10 @@
+import { MdOutlineVideoCall } from "react-icons/md";
+
 const VideoCall = () => {
     return (
         <>
-            <h1>Video</h1>
+            <h1>Video Call</h1>
+            <span><MdOutlineVideoCall /></span>
         </>
     )
 }
