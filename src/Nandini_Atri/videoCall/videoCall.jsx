ddf82a1b888b359135dad84fdff4e7f5ee6be7d8
Nandiniatri <atrinandini8@gmail.com>
2025-05-17 15:45:18 +0530

    right a code in videocall and insert css

--- src/Nandini_Atri/videoCall/videoCall.jsx
+++ src/Nandini_Atri/videoCall/videoCall.jsx
@@ -2,11 +2,19 @@ import { MdOutlineVideoCall } from "react-icons/md";
 
 const VideoCall = () => {
     return (
-        <>
-            <h1>Video Call</h1>
-            <span><MdOutlineVideoCall /></span>
-        </>
-    )
-}
+        <div className="videocall-main-div">
+            <div className="videocall-header">
+                <h1 className="videocall-title">Start a Video Call</h1>
+                <button className="videoCall-button">
+                    <MdOutlineVideoCall size={30} />
+                </button>
+            </div>
+
+            <div className="videocall-body">
+                <p>Video Call Here !</p>
+            </div>
+        </div>
+    );
+};
 
 export default VideoCall;
