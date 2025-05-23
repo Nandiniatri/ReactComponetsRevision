dfcc96fc645af3bdb289f645444b12c835b8948b
Nandiniatri <atrinandini8@gmail.com>
2025-05-23 15:53:52 +0530

    app.jsx file remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
-import NandiniAtri from './Nandini_Atri/nandiniATri'
-import NewVideoCall from './Nandini_Atri/newVideoCallFolder/newVideoCall'
-import VideoCalls from './Nandini_Atri/videoCall'
 
 function App() {
 
   return (
     <>
-      {/* <NandiniAtri /> */}
-      {/* <VideoCalls /> */}
-      <NewVideoCall />
     </>
   )
 }
